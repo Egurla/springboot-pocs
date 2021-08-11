fd0122512b60885f43a363cc68b360d512f1f993
Ravi Egurla <regurla@nisum.com>
2021-08-11 12:59:17 +0530

    spring boot

--- src/test/java/com/nisum/cms/CartManagementApplicationTests.java
+++ src/test/java/com/nisum/cms/CartManagementApplicationTests.java
@@ -1,16 +1,9 @@
 package com.nisum.cms;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-//@SpringBootConfiguration
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = CartManagementApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class CartManagementApplicationTests {
 
 	@Test
